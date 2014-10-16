4db356266e368c15c2081e07c851244ea17de1e4
zengg <gzeng@dbrs.com>
2014-10-16 10:00:10 -0400

    Create the C# Project and Solution.
    Planning to put everything into one project for maximum speed.

---
+++ Source/Chester3/Chester3/Common/Chess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chester3.Common
+{
+    public static class Chess
+    {
+        public enum Pieces : byte { KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN };
+    }
+}
---
+++ Source/Chester3/Chester3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chester3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
