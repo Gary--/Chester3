556a625b7769997fc452b3ae40ab0a6a52e6a94a
Gary <gary_zeng@hotmail.com>
2014-11-01 14:10:18 -0400

    Rename the correctness perft test class.

--- Source/Engine.Tests/Test_Move_Generation_perft.cpp
+++ Source/Engine.Tests/Test_Move_Generation_perft.cpp
@@ -7,7 +7,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 
 namespace EngineTests {
-	TEST_CLASS(Test_Basic_Perft) {
+	TEST_CLASS(Test_Move_Generation_Perft) {
 public:
 	
 	int mirrorLazyPerft(const char* FEN,int depth) {
