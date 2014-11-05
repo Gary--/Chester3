647873d426b882e1023a0ff4b44a5082d407f998
zengg <gzeng@dbrs.com>
2014-11-05 11:52:54 -0500

    Add additional tests for tacticalOnly move generation.

--- Source/Engine.Tests/Test_Move_generation.cpp
+++ Source/Engine.Tests/Test_Move_generation.cpp
@@ -36,6 +36,7 @@ public:
 		Assert::AreEqual(0, numTacticalMoves("8/4k3/8/8/8/8/8/R3K2R w KQ -"));
 	}
 
+
 	TEST_METHOD(Generate_All_After_Tactical_Only) {
 		const char* FEN = "8/2P5/8/8/8/8/8/5k1K w - -";
 		Game::configure(GameConfiguration(FEN));
@@ -70,6 +71,50 @@ public:
 		Assert::AreEqual(4, numTactical);
 	}
 
+	TEST_METHOD(Generate_Tactical_MakeUnMake_Generate_All) {
+		const char* FEN = "8/2P5/8/8/8/8/8/5k1K w - -";
+		Game::configure(GameConfiguration(FEN));
+
+		int numTactical = 0;
+		for (Move move : Game::getTacticalMoves()) {
+			numTactical++;
+
+			if (move.isPromotion() && move.promotionPiece()==Piece::QUEEN) {
+				Game::makeMove(move);
+				Game::getAllMoves();
+				Game::undoMove();
+			}
+		}
+		Assert::AreEqual(4, numTactical);
+
+		int numTotal = 0;
+		for (Move move : Game::getAllMoves()) {
+			numTotal++;
+		}
+		Assert::AreEqual(5, numTotal);
+	}
+
+	TEST_METHOD(Generate_All_MakeUnMake_Generate_Tactical) {
+		const char* FEN = "8/2P5/8/8/8/8/8/5k1K w - -";
+		Game::configure(GameConfiguration(FEN));
+
+		int numTotal = 0;
+		for (Move move : Game::getAllMoves()) {
+			numTotal++;
+			if (move.getPiece() == Piece::KING) {
+				Game::makeMove(move);
+				Game::getAllMoves();
+				Game::undoMove();
+			}
+		}
+		Assert::AreEqual(5, numTotal);
+
+		int numTactical = 0;
+		for (Move move : Game::getTacticalMoves()) {
+			numTactical++;
+		}
+		Assert::AreEqual(4, numTactical);
+	}
 
 	};
 }
