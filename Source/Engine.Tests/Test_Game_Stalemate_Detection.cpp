0551f856f4923210cc993a9f2fc161218aa104bc
Gary <gary_zeng@hotmail.com>
2014-11-05 23:46:24 -0500

    Fast (non) stale mate detection and tests.

---
+++ Source/Engine.Tests/Test_Game_Stalemate_Detection.cpp
@@ -0,0 +1,60 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Move.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+
+namespace EngineTests {
+	TEST_CLASS(Test_Game_Stalemate_Detection) {
+public:
+
+	void testHasMove(const char* FEN, bool hasMove) {
+		GameConfiguration conf(FEN);
+
+		Game::configure(conf);
+		Assert::AreEqual(hasMove, Game::areMovesAvailable());
+
+		Game::configure(conf.mirror());
+		Assert::AreEqual(hasMove, Game::areMovesAvailable());
+	}
+	void yes(const char* FEN) {
+		testHasMove(FEN, true);
+	}
+	void no(const char* FEN) {
+		testHasMove(FEN, false);
+	}
+
+	TEST_METHOD(Starting_Position_Has_Move) {
+		yes("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - -");
+	}
+
+	TEST_METHOD(Stalemate_0) {
+		no("8/8/8/8/8/8/7R/k1K5 b - -");
+	}
+
+	TEST_METHOD(Stalemate_1) {
+		no("8/8/8/p7/P7/8/7R/k1K5 b - -");
+	}
+
+	TEST_METHOD(Stalemate_2) {
+		no("8/8/2p1p3/1pP1Pp2/1N1n1P2/1p3p2/1Np1pN1R/k1K1Q3 b - -");
+	}
+
+	TEST_METHOD(Stalemate_3) {
+		no("4R3/8/8/8/R7/5kpp/4p1pp/4K1nq b - - ");
+	}
+	
+	TEST_METHOD(Stalemate_4) {
+		no("7b/8/8/8/8/k7/1R1n4/K7 w - - ");
+	}
+
+	TEST_METHOD(Stalemate_5) {
+		no("2r1r3/8/8/r7/3KB2r/r7/8/6k1 w - -")
+	}
+
+	};
+
+
+}
--- Source/Engine/Game_stalemate_detection.cpp
+++ Source/Engine/Game_stalemate_detection.cpp
@@ -1,8 +1,53 @@
 #include "Game.h"
+#include "attack_fields.h"
+
+using namespace AttackFields;
 
 bool Game::isDefinitelyAMoveAvailable() {
-	//if (getCheck) {
+	if (getCheck()) { // Should probably be dealing with the check..
+		return false;
+	}
+	
+	Position kingPos = getPieces(getTurn(), Piece::KING).ToPosition();
+	BitBoard kingSees = queenTargs(kingPos, ALL);
+	
 
-	//}
+	BitBoard freePawnsForward = (getPieces(getTurn(), Piece::PAWN) &~kingSees).shiftForward(getTurn());
+
+	// A pawn can move forward
+	if ((freePawnsForward&~ALL) != BitBoard::EMPTY()) {
+		return true;
+	}
+
+	// A pawn can capture
+	if (((freePawnsForward.shiftLeft() | freePawnsForward.shiftRight())
+		&getPlayerPieces(!getTurn())) != BitBoard::EMPTY()) {
+		return true;
+	}
+
+	BitBoard M = getPlayerPieces(getTurn());
+
+	// A knight can move
+	if ((knightTargs(getPieces(getTurn(), Piece::KNIGHT)&~kingSees)&~M)!=BitBoard::EMPTY()) {
+		return true;
+	}
+
+	// A rook can move
+	BitBoard MR = (getPieces(getTurn(), Piece::ROOK) | getPieces(getTurn(), Piece::QUEEN))&~kingSees;
+	if (MR != BitBoard::EMPTY()) {
+		if (((MR.shiftUp() | MR.shiftDown() | MR.shiftLeft() | MR.shiftRight())&~M) != BitBoard::EMPTY()) {
+			return true;
+		}
+	}
+
+	// A bishop can move
+	BitBoard MB = (getPieces(getTurn(), Piece::BISHOP) | getPieces(getTurn(), Piece::QUEEN))&~kingSees;
+	if (MB != BitBoard::EMPTY()) {
+		BitBoard vert = MB.shiftUp() | MB.shiftDown();
+
+		if (((vert.shiftLeft() | vert.shiftRight())&~M) != BitBoard::EMPTY()) {
+			return true;
+		}
+	}
 	return false;
 }
