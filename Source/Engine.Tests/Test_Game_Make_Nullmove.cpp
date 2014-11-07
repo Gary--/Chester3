c36548a79b4a1dff295750b54132b72177bcda60
Gary <gary_zeng@hotmail.com>
2014-11-06 21:13:43 -0500

    Add nullmove support in Game and tests.

---
+++ Source/Engine.Tests/Test_Game_Make_Nullmove.cpp
@@ -0,0 +1,60 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Game.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+
+namespace EngineTests {
+	TEST_CLASS(Test_Game_Make_Nullmove) {
+public:
+
+	TEST_METHOD(Nullmove_Flips_Turn) {
+		Game::configure(GameConfiguration("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - "));
+		Game::makeMove(Move::NULL_MOVE());
+		Assert::AreEqual(Turn::BLACK, Game::getTurn());
+		Game::makeMove(Move::NULL_MOVE());
+		Assert::AreEqual(Turn::WHITE, Game::getTurn());
+		Game::undoMove();
+		Assert::AreEqual(Turn::BLACK, Game::getTurn());
+		Game::undoMove();
+		Assert::AreEqual(Turn::WHITE, Game::getTurn());
+	}
+
+	TEST_METHOD(Nullmove_Flips_Hash) {
+		Game::configure(GameConfiguration("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - "));
+		uint64_t orig = Game::getHash();
+
+		Game::makeMove(Move::NULL_MOVE());
+		Assert::AreEqual(~orig, Game::getHash());
+
+		Game::makeMove(Move::NULL_MOVE());
+		Assert::AreEqual(orig, Game::getHash());
+	}
+
+	TEST_METHOD(NullMove_Voids_Enpeasant) {
+		std::string FEN = "8/3k4/8/8/5pP1/8/8/3K4 b - g3";
+		Game::configure(GameConfiguration(FEN.c_str()));
+		Assert::AreEqual(6, Game::getEnpeasentColumn());
+
+		Game::makeMove(Move::NULL_MOVE());
+		Assert::AreEqual(GameConfiguration::NO_ENPEASENT_COLUMN, Game::getEnpeasentColumn());
+		
+		Game::undoMove();
+
+		Assert::AreEqual(FEN, GameConfiguration::extractFromGame().str_min());
+	}
+
+	TEST_METHOD(Move_Generation_Works_After_Nullmove) {
+		Game::configure(GameConfiguration("k7/8/8/8/8/8/8/K7 w - -"));
+		Game::makeMove(Move::NULL_MOVE());
+		int n = 0;
+		for (Move move : Game::getAllMoves()) {
+			n++;
+		}
+		Assert::AreEqual(3, n);
+	}
+
+	};
+}
--- Source/Engine/Game_make_move.cpp
+++ Source/Engine/Game_make_move.cpp
@@ -2,132 +2,136 @@
 #include "attack_fields.h"
 
 void Game::makeMove(Move move) {
-
+	bool wasInCheck = getCheck();
 	pushMove(move);
+	if (move == Move::NULL_MOVE()) {
+		_ASSERTE(!wasInCheck);
+		cur.hash.setEnpeasent(GameConfiguration::NO_ENPEASENT_COLUMN);
+
+	} else {
 
 #pragma region Unpack move
-	const MoveType type = move.getType();
-	const Position fromPos = move.getFrom();
-	const BitBoard fromBit = fromPos.ToSingletonBoard();
-	const Position toPos = move.getTo();
-	const BitBoard toBit = toPos.ToSingletonBoard();
-	const Piece piece = move.getPiece();
-	const Piece targ = move.getTarg();
+		const MoveType type = move.getType();
+		const Position fromPos = move.getFrom();
+		const BitBoard fromBit = fromPos.ToSingletonBoard();
+		const Position toPos = move.getTo();
+		const BitBoard toBit = toPos.ToSingletonBoard();
+		const Piece piece = move.getPiece();
+		const Piece targ = move.getTarg();
 #pragma endregion
-	
-	
 
-	_ASSERTE(!sp(curTurn)->contains(toPos));
+
+
+		_ASSERTE(!sp(curTurn)->contains(toPos));
 
 #pragma region Normal Move
-	ALL ^= fromBit;
-	ALL |= toBit;
-	setPieceAt(fromPos, Piece::EMPTY);
-	setPieceAt(toPos, piece);
-	
-	*sp(curTurn) ^= fromBit | toBit;
-	*sp(!curTurn) &= ~toBit;
+		ALL ^= fromBit;
+		ALL |= toBit;
+		setPieceAt(fromPos, Piece::EMPTY);
+		setPieceAt(toPos, piece);
 
-	*s(curTurn, piece) ^= fromBit | toBit;
-	cur.hash.togglePiece(fromPos,curTurn,piece);
-	cur.hash.togglePiece(toPos, curTurn, piece);
+		*sp(curTurn) ^= fromBit | toBit;
+		*sp(!curTurn) &= ~toBit;
 
-	*s(!curTurn, targ) ^= toBit;
-	cur.hash.togglePiece(toPos, !curTurn, targ);
+		*s(curTurn, piece) ^= fromBit | toBit;
+		cur.hash.togglePiece(fromPos, curTurn, piece);
+		cur.hash.togglePiece(toPos, curTurn, piece);
 
-	cur.hash.setEnpeasent(GameConfiguration::NO_ENPEASENT_COLUMN);
+		*s(!curTurn, targ) ^= toBit;
+		cur.hash.togglePiece(toPos, !curTurn, targ);
+
+		cur.hash.setEnpeasent(GameConfiguration::NO_ENPEASENT_COLUMN);
 #pragma endregion
 
 
 
-	if (type == MoveType::PAWN_JUMP) { // Make sure the move enpeasent is actually legal
-		BitBoard enpeasentFrom = toBit.shiftLeft() | toBit.shiftRight();
-		FOR_BIT(pawn, getPieces(!curTurn, Piece::PAWN) & enpeasentFrom) {
-			Position theirKingPos = getPieces(!curTurn, Piece::KING).ToPosition();
-			BitBoard newBlockers = getAllPieces() ^ pawn^toBit^
-				AttackFields::enpeasentTo(!curTurn, toPos.col()).ToSingletonBoard();
-			BitBoard diagProblems = (getPieces(curTurn, Piece::QUEEN) | getPieces(curTurn, Piece::BISHOP))&
-				AttackFields::bishopTargs(theirKingPos, newBlockers);
-			BitBoard rightProblems = (getPieces(curTurn, Piece::QUEEN) | getPieces(curTurn, Piece::ROOK))&
-				AttackFields::rookTargs(theirKingPos, newBlockers);
-			
-			if ((diagProblems | rightProblems) == BitBoard::EMPTY()) {
-				cur.hash.setEnpeasent(toPos.col());
-				break;
+		if (type == MoveType::PAWN_JUMP) { // Make sure the move enpeasent is actually legal
+			BitBoard enpeasentFrom = toBit.shiftLeft() | toBit.shiftRight();
+			FOR_BIT(pawn, getPieces(!curTurn, Piece::PAWN) & enpeasentFrom) {
+				Position theirKingPos = getPieces(!curTurn, Piece::KING).ToPosition();
+				BitBoard newBlockers = getAllPieces() ^ pawn^toBit^
+					AttackFields::enpeasentTo(!curTurn, toPos.col()).ToSingletonBoard();
+				BitBoard diagProblems = (getPieces(curTurn, Piece::QUEEN) | getPieces(curTurn, Piece::BISHOP))&
+					AttackFields::bishopTargs(theirKingPos, newBlockers);
+				BitBoard rightProblems = (getPieces(curTurn, Piece::QUEEN) | getPieces(curTurn, Piece::ROOK))&
+					AttackFields::rookTargs(theirKingPos, newBlockers);
+
+				if ((diagProblems | rightProblems) == BitBoard::EMPTY()) {
+					cur.hash.setEnpeasent(toPos.col());
+					break;
+				}
+			}
+		} else if (type == MoveType::ENPEASENT) {
+			BitBoard capturedBit = toBit.shiftBackward(curTurn);
+			Position capturedPos = capturedBit.ToPosition();
+
+			toggleBit(!curTurn, capturedPos, Piece::PAWN);
+			setPieceAt(capturedPos, Piece::EMPTY);
+			cur.hash.togglePiece(capturedPos, !curTurn, Piece::PAWN);
+
+		} else if (move.isPromotion()) {
+			Piece promo = move.promotionPiece();
+
+			*s(curTurn, Piece::PAWN) ^= toBit;
+			cur.hash.togglePiece(toPos, curTurn, Piece::PAWN);
+
+			*s(curTurn, promo) ^= toBit;
+			cur.hash.togglePiece(toPos, curTurn, promo);
+			setPieceAt(toPos, promo);
+		} else if (piece == Piece::KING) {
+			cur.hash.voidCastle(curTurn, Side::LEFT);
+			cur.hash.voidCastle(curTurn, Side::RIGHT);
+
+			if (type == MoveType::CASTLE_LEFT || type == MoveType::CASTLE_RIGHT) {
+				int baseRow = curTurn == Turn::WHITE ? 7 : 0;
+				Position rookFrom(baseRow, type == MoveType::CASTLE_LEFT ? 0 : 7);
+				Position rookTo(baseRow, type == MoveType::CASTLE_LEFT ? 3 : 5);
+				BitBoard rookChange = rookFrom.ToSingletonBoard() | rookTo.ToSingletonBoard();
+
+				ALL ^= rookChange;
+				*sp(curTurn) ^= rookChange;
+				*s(curTurn, Piece::ROOK) ^= rookChange;
+				setPieceAt(rookFrom, Piece::EMPTY);
+				setPieceAt(rookTo, Piece::ROOK);
+
+				cur.hash.togglePiece(rookFrom, curTurn, Piece::ROOK);
+				cur.hash.togglePiece(rookTo, curTurn, Piece::ROOK);
 			}
 		}
-	} else if (type == MoveType::ENPEASENT) {
-		BitBoard capturedBit = toBit.shiftBackward(curTurn);
-		Position capturedPos = capturedBit.ToPosition();
 
-		toggleBit(!curTurn, capturedPos, Piece::PAWN);
-		setPieceAt(capturedPos, Piece::EMPTY);
-		cur.hash.togglePiece(capturedPos, !curTurn, Piece::PAWN);
-
-	} else if (move.isPromotion()) {
-		Piece promo = move.promotionPiece();
-
-		*s(curTurn, Piece::PAWN) ^= toBit;
-		cur.hash.togglePiece(toPos, curTurn, Piece::PAWN);
-
-		*s(curTurn, promo) ^= toBit;
-		cur.hash.togglePiece(toPos, curTurn, promo);
-		setPieceAt(toPos, promo);
-	} else if (piece == Piece::KING) {
-		cur.hash.voidCastle(curTurn, Side::LEFT);
-		cur.hash.voidCastle(curTurn, Side::RIGHT);
-
-		if (type == MoveType::CASTLE_LEFT || type== MoveType::CASTLE_RIGHT) {
-			int baseRow = curTurn == Turn::WHITE ? 7 : 0;
-			Position rookFrom(baseRow, type == MoveType::CASTLE_LEFT ? 0 : 7);
-			Position rookTo(baseRow, type == MoveType::CASTLE_LEFT ? 3 : 5);
-			BitBoard rookChange = rookFrom.ToSingletonBoard() | rookTo.ToSingletonBoard();
-
-			ALL ^= rookChange;
-			*sp(curTurn) ^= rookChange;
-			*s(curTurn, Piece::ROOK) ^= rookChange;
-			setPieceAt(rookFrom, Piece::EMPTY);
-			setPieceAt(rookTo, Piece::ROOK);
-
-			cur.hash.togglePiece(rookFrom, curTurn, Piece::ROOK);
-			cur.hash.togglePiece(rookTo, curTurn, Piece::ROOK);
+		if (toPos == Position("a1") || fromPos == Position("a1")) {
+			cur.hash.voidCastle(Turn::WHITE, Side::LEFT);
 		}
-	}
+		if (toPos == Position("h1") || fromPos == Position("h1")) {
+			cur.hash.voidCastle(Turn::WHITE, Side::RIGHT);
+		}
+		if (toPos == Position("a8") || fromPos == Position("a8")) {
+			cur.hash.voidCastle(Turn::BLACK, Side::LEFT);
+		}
+		if (toPos == Position("h8") || fromPos == Position("h8")) {
+			cur.hash.voidCastle(Turn::BLACK, Side::RIGHT);
+		}
 
-	if (toPos == Position("a1") || fromPos == Position("a1")) {
-		cur.hash.voidCastle(Turn::WHITE, Side::LEFT);
-	}
-	if (toPos == Position("h1") || fromPos == Position("h1")) {
-		cur.hash.voidCastle(Turn::WHITE, Side::RIGHT);
-	}
-	if (toPos == Position("a8") || fromPos == Position("a8")) {
-		cur.hash.voidCastle(Turn::BLACK, Side::LEFT);
-	}
-	if (toPos == Position("h8") || fromPos == Position("h8")) {
-		cur.hash.voidCastle(Turn::BLACK, Side::RIGHT);
-	}
 
-	
-	_ASSERTE(!posAttackedBy(getPieces(curTurn, Piece::KING).ToPosition(), !curTurn));
+		_ASSERTE(!posAttackedBy(getPieces(curTurn, Piece::KING).ToPosition(), !curTurn));
 
-	
-	BitBoard theirKingBit = getPieces(!curTurn, Piece::KING);
 
-	cur.check = false;
-	if ((piece == Piece::PAWN && (AttackFields::pawnTargs(toPos,curTurn)&theirKingBit) != BitBoard::EMPTY()) ||
-		((piece == Piece::KNIGHT || (move.isPromotion() && move.promotionPiece()==Piece::KNIGHT)) &&
-		(AttackFields::knightTargs(toPos)&theirKingBit) != BitBoard::EMPTY())) {
-		cur.check = true;
+		BitBoard theirKingBit = getPieces(!curTurn, Piece::KING);
+
+		cur.check = false;
+		if ((piece == Piece::PAWN && (AttackFields::pawnTargs(toPos, curTurn)&theirKingBit) != BitBoard::EMPTY()) ||
+			((piece == Piece::KNIGHT || (move.isPromotion() && move.promotionPiece() == Piece::KNIGHT)) &&
+			(AttackFields::knightTargs(toPos)&theirKingBit) != BitBoard::EMPTY())) {
+			cur.check = true;
+		}
+		if (cur.check == false && posAttackedByLOS(theirKingBit.ToPosition(), curTurn)) {
+			cur.check = true;
+		}
+
 	}
-	if (cur.check == false && posAttackedByLOS(theirKingBit.ToPosition(), curTurn)) {
-		cur.check = true;
-	}
-
 	
 	curTurn = !curTurn;
-	
 	cur.hash.toggleTurn();
-
 	integrityCheck();
 	
 }
@@ -140,10 +144,11 @@ void Game::undoMove() {
 	//====
 #pragma region Unpack Undo Data
 	popMove();
-
-
-
 	Move move = cur.move;
+	if (move == Move::NULL_MOVE()) {
+		return;
+	}
+
 	const MoveType type = move.getType();
 	const Position fromPos = move.getFrom();
 	const BitBoard fromBit = fromPos.ToSingletonBoard();
