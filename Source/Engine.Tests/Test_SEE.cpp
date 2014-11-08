b87b605edccb8127f15daeb6945a88b0e85367d4
Gary <gary_zeng@hotmail.com>
2014-11-08 13:17:00 -0500

    Add additional SEE tests.

--- Source/Engine.Tests/Test_SEE.cpp
+++ Source/Engine.Tests/Test_SEE.cpp
@@ -30,9 +30,80 @@ public:
 			FOR_PIECE_ALL(attacker) {
 				AtkPat attackers;
 				attackers.add(attacker);
+
 				Assert::IsTrue(SEE::attackCost(attacker, attackers, defenders) > 0);
 			}
 		}
+	}
+
+	TEST_METHOD(Defended_Pawn) {
+		FOR_PIECE_ALL(piece) {
+			AtkPat attackers, defenders;
+			attackers.add(piece);
+			defenders.add(piece);
+
+			attackers.add(Piece::PAWN);
+			if (SEE::attackCost(Piece::PAWN, attackers, defenders)) {
+				Assert::AreEqual(0, SEE::attackCost(Piece::PAWN, attackers, defenders));
+			}
+
+			Assert::AreEqual(0, SEE::attackCost(Piece::PAWN, attackers, defenders));
+		}
+	}
+
+	// attacker will be added to the pattern
+	int exchangeValue(char attacker0, std::string pat) {
+		Piece attacker = ChessUtils::pieceFromChar(attacker0);
+
+		AtkPat attackers, defenders;
+		attackers.add(attacker);
+
+		for (char c : pat) {
+			Piece piece = ChessUtils::pieceFromChar(c);
+			Turn turn = ChessUtils::turnFromChar(c);
+			(turn == Turn::WHITE ? attackers : defenders).add(piece);
+		}
+		return SEE::attackCost(attacker, attackers, defenders);
+	}
+
+	TEST_METHOD(SEE_Q_Pr) {
+		Assert::AreEqual(4, exchangeValue('Q', "Pr"));
+	}
+
+	TEST_METHOD(SEE_P_PQpr) {
+		Assert::AreEqual(0, exchangeValue('P', "PQpr"));
+	}
+
+	TEST_METHOD(SEE_R_RQnr) {
+		Assert::AreEqual(2, exchangeValue('R', "RQnr"));
+	}
+
+	TEST_METHOD(SEE_R_Qbp) {
+		Assert::AreEqual(5, exchangeValue('R', "Qbp"));
+	}
+
+	TEST_METHOD(SEE_P_RQbp) {
+		Assert::AreEqual(1, exchangeValue('P', "RQbp"));
+	}
+
+	TEST_METHOD(SEE_R_Rqq) {
+		Assert::AreEqual(1, exchangeValue('R', "Rqk"));
+	}
+
+	TEST_METHOD(SEE_B_Brk) {
+		
+	}
+
+	TEST_METHOD(SEE_Even_Fight_Over_Pawn) {
+		Assert::AreEqual(0, exchangeValue('P', "NRQKnrqk"));
+	}
+
+	TEST_METHOD(SEE_Winning_Fight_Over_Pawn) {
+		Assert::AreEqual(0, exchangeValue('P', "NRQKnqk"));
+	}
+
+	TEST_METHOD(SEE_Losing_Fight_Over_Pawn) {
+		Assert::AreEqual(1, exchangeValue('P', "NRKnrqk"));
 	}
 
 	};
