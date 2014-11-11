9b5fe301a77fefb9b29222fd6cbb8823f307374a
Gary-- <gary_zeng@hotmail.com>
2014-11-11 18:50:07 -0500

    Add major fields to SearchConfiguration

--- Source/Engine/AI_search_configuration.h
+++ Source/Engine/AI_search_configuration.h
@@ -1,5 +1,13 @@
 #pragma once
 
 struct SearchConfiguration {
+	int searchTimeMs;
+	bool mateOnly;
+	int maxDepth;
+
+	static const int MAX_DEPTH_INF = -1;
+	static const int SEARCH_TIME_INF = -1;
+
+private:
 
 };
