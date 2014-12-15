68778bdf8701580ba1176653e6e1c79c894535db
zengg <gzeng@dbrs.com>
2014-12-15 14:03:31 -0500

    Implement EvaluationManager.

---
+++ Source/Engine/EvaluationManager.cpp
@@ -0,0 +1,41 @@
+#include "EvaluationManager.h"
+#include "Evaluation.h"
+#include <vector>
+
+using namespace std;
+namespace {
+	vector<EvaluationScore> scores(1, EvaluationScore::GetInvalid());
+}
+
+void EvaluationManager::synchronize() {
+	Evaluation::synchronize();
+	scores = vector<EvaluationScore>(1, EvaluationScore::GetInvalid());
+}
+
+void EvaluationManager::notifyMove(Move move, Turn turn) {
+	Evaluation::notifyMove(move, turn);
+	scores.push_back(EvaluationScore::GetInvalid());
+}
+
+void EvaluationManager::notifyUndoMove(Move move, Turn turn) {
+	Evaluation::notifyUndoMove(move, turn);
+	scores.pop_back();
+}
+
+EvaluationScore EvaluationManager::getScore(int n) {
+	if (n == 0) {
+		calcScoreCurrent();
+	}
+	_ASSERTE(n < scores.size());
+	return scores.at(scores.size() - 1 - n);
+}
+
+EvaluationScore EvaluationManager::getScore() {
+	return getScore(0);
+}
+
+void EvaluationManager::calcScoreCurrent() {
+	if (!scores.back().isValid()) {
+		scores.back() = EvaluationScore::GetFromGameState();
+	}
+}
--- Source/Engine/EvaluationManager.h
+++ Source/Engine/EvaluationManager.h
@@ -1,18 +1,22 @@
 #pragma once
 #include "Move.h"
 #include <cstdint>
-
+#include "EvaluationScore.h"
 class EvaluationManager {
 public:
-	static void sync();
+	static void synchronize();
 	static void notifyMove(Move move, Turn turn);
 	static void notifyUndoMove(Move move, Turn turn);
 
-	static const int UNKNOWN_EVAL_SCORE = INT_MIN;
+	// Save the score of the current state
+	static void calcScoreCurrent();
 
-	// Get the score from n turns ago. n=0 is the current turn
-	static int getScore(Turn turn,int n);
+	// Get the score from n turns ago. n=0 is the current state.
+	static EvaluationScore getScore(int n);
 
+	// Get the score for the current state.
+	static EvaluationScore getScore();
 
 private:
+	
 };
