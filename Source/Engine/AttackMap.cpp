d556e5f97137ae50fd28cd771233248e4f3bb83a
Gary <gary_zeng@hotmail.com>
2014-12-28 16:47:07 -0500

    Optimize AttackMap.
    Only one set of maps is saved now.

--- Source/Engine/AttackMap.cpp
+++ Source/Engine/AttackMap.cpp
@@ -8,82 +8,74 @@
 using namespace std;
 
 namespace {
-	struct AttackMapSaved {
-	public:
-		AttackPattern patterns[2][64];
-		bool precomputed;
 
-		AttackMapSaved() {
-			precomputed = false;
-		}
+	// bit n of i is on <==> bit 8*n of rows[i] is on.
+	AttackPattern patterns[2][64];
+	uint64_t savedHash=0;
 
-		void add(const Turn turn,const Piece piece,const BitBoard cover) {
-			FOR_8(r) {
-				uint64_t coverRowBits = (cover.asInt64() >> (8 * r)) & 0xFFULL;
+	uint64_t rows[256] = { 0 };
+	bool inited = false;
 
-				if (coverRowBits) {
-					const uint64_t rowMask = rows[coverRowBits];
 
-					uint64_t* rowPtr = reinterpret_cast <uint64_t*>(&patterns[turn.asIndex()][8 * r]);
-					*rowPtr |= rowMask << AttackPattern::pieceBitLocation(piece);
-					*rowPtr += rowMask;
-				}
-				
-			}
+	void addPat(const Turn turn, const Piece piece, const BitBoard cover) {
+		FOR_8(r) {
+			uint64_t coverRowBits = (cover.asInt64() >> (8 * r)) & 0xFFULL;
 
-			
-		}
+			if (coverRowBits) {
+				const uint64_t rowMask = rows[coverRowBits];
 
-		static void init() {
-			if (inited) {
-				return;
-			}
-			inited = true;
-
-			for (uint8_t x = 0;; x++) {
-				for (int col = 0; col < 8;++col)
-				{
-					if (x & (1 << col)) {
-						rows[x] |= ((uint64_t)1 << (col * 8));
-					}
-				}
-
-				if (x == 255) {
-					break;
-				}
+				uint64_t* rowPtr = reinterpret_cast <uint64_t*>(&patterns[turn.asIndex()][8 * r]);
+				*rowPtr |= rowMask << AttackPattern::pieceBitLocation(piece);
+				*rowPtr += rowMask;
 			}
 		}
-	private:
-		static bool inited;
+	}
 
-		// bit n of i is on <==> bit 8*n of rows[i] is on.
-		static uint64_t rows[256];
-	};
-	uint64_t AttackMapSaved::rows[256] = { 0 };
-	bool AttackMapSaved::inited = false;
+	void resetPats() {
+		memset(patterns, 0, sizeof(patterns));
+	}
 
-	vector<AttackMapSaved> maps;
+
+	static void init() {
+		if (inited) {
+			return;
+		}
+		inited = true;
+
+		for (uint8_t x = 0;; x++) {
+			for (int col = 0; col < 8; ++col) {
+				if (x & (1 << col)) {
+					rows[x] |= ((uint64_t)1 << (col * 8));
+				}
+			}
+
+			if (x == 255) {
+				break;
+			}
+		}
+	}
+
+	
 }
 
 void AttackMap::synchronize() {
-	AttackMapSaved::init();
-
-	maps.clear();
-	maps.push_back(AttackMapSaved());
+	init();
+	return;
 }
 
 void AttackMap::notifyMove(const Move move, const  Turn turn) {
-	maps.push_back(AttackMapSaved());
+	return;
 }
 
 void AttackMap::notifyUndoMove(const Move move, const  Turn turn) {
-	maps.pop_back();
+	return;
 }
 
 AttackPattern AttackMap::getAttackPattern(const Turn turn, const  Position position) {
-	if (maps.back().precomputed) {
-		return maps.back().patterns[turn.asIndex()][position.index()];;
+	if (savedHash == Game::getHash()) {
+		return patterns[turn.asIndex()][position.index()];;
 	}
+
 
 	return getAttackPatternImpl(turn, position);
 }
@@ -124,22 +116,21 @@ AttackPattern AttackMap::getAttackPatternImpl(const Turn turn, const  Position p
 }
 
 void AttackMap::precompute() {
-	if (maps.back().precomputed) {
+	if (savedHash == Game::getHash()) {
 		return;
 	}
-	maps.back().precomputed = true;
+	savedHash = Game::getHash();
 
-	auto& pats = maps.back();
-	//maps.back().clearPatterns();
-	
+
+	resetPats();
 	FOR_TURN(turn) {
 		//FOR_POS(kingPos, AttackFields::kingTargs(Game::getKingPosition(turn))) {
 		//	pats.patterns[turn.asIndex()][kingPos.index()].add(Piece::KING());
 		//}
-		pats.add(turn, Piece::KING(), AttackFields::kingTargs(Game::getKingPosition(turn)));
+		addPat(turn, Piece::KING(), AttackFields::kingTargs(Game::getKingPosition(turn)));
 
 		FOR_POS(knightPos, Game::getPieces(turn, Piece::KNIGHT())) {
-			pats.add(turn, Piece::KNIGHT(), AttackFields::knightTargs(knightPos));
+			addPat(turn, Piece::KNIGHT(), AttackFields::knightTargs(knightPos));
 			//FOR_POS(targ, AttackFields::knightTargs(knightPos)) {
 			//	pats.patterns[turn.asIndex()][targ.index()].add(Piece::KNIGHT());
 			//}
@@ -148,31 +139,33 @@ void AttackMap::precompute() {
 		const BitBoard blockers = Game::getAllPieces();
 
 		FOR_POS(bishopPos, Game::getPieces(turn, Piece::BISHOP())) {
-			pats.add(turn, Piece::BISHOP(), AttackFields::bishopTargs(bishopPos, blockers));
+			addPat(turn, Piece::BISHOP(), AttackFields::bishopTargs(bishopPos, blockers));
 			//FOR_POS(targ, AttackFields::bishopTargs(bishopPos, blockers)) {
 			//	pats.patterns[turn.asIndex()][targ.index()].add(Piece::BISHOP());
 			//}
 		}
 
 		FOR_POS(rookPos, Game::getPieces(turn, Piece::ROOK())) {
-			pats.add(turn, Piece::ROOK(), AttackFields::rookTargs(rookPos, blockers));
+			addPat(turn, Piece::ROOK(), AttackFields::rookTargs(rookPos, blockers));
 			//FOR_POS(targ, AttackFields::rookTargs(rookPos, blockers)) {
 			//	pats.patterns[turn.asIndex()][targ.index()].add(Piece::ROOK());
 			//}
 		}
 
 		FOR_POS(queenPos, Game::getPieces(turn, Piece::QUEEN())) {
-			pats.add(turn, Piece::QUEEN(), AttackFields::queenTargs(queenPos, blockers));
+			addPat(turn, Piece::QUEEN(), AttackFields::queenTargs(queenPos, blockers));
 			//FOR_POS(targ, AttackFields::queenTargs(queenPos, blockers)) {
 			//	pats.patterns[turn.asIndex()][targ.index()].add(Piece::QUEEN());
 			//}
 		}
 
+		// 1 sqr in front of our pawns
 		const BitBoard MP_forward = Game::getPieces(turn, Piece::PAWN()).shiftForward(turn);
-		const BitBoard left = MP_forward.shiftLeft();
-		const BitBoard right = MP_forward.shiftRight();
-		pats.add(turn, Piece::PAWN(), left);
-		pats.add(turn, Piece::PAWN(), right);
+
+		addPat(turn, Piece::PAWN(), MP_forward.shiftLeft());
+		addPat(turn, Piece::PAWN(), MP_forward.shiftRight());
+
+
 		//const BitBoard both = left&right;
 
 		//FOR_POS(targ, (left | right) &~both) {
--- Source/Engine/AttackMap.h
+++ Source/Engine/AttackMap.h
@@ -7,9 +7,13 @@
 // setPrecompute will efficiently compute the AttackPattern for every square
 class AttackMap {
 public:
+	// Initialize internal data structures.
 	static void synchronize();
 
+	// Precompute for the current Game hash. Only 1 will be stored.
 	static void precompute();
+
+	
 	static void notifyMove(Move move, Turn turn);
 	static void notifyUndoMove(Move move, Turn turn);
 
